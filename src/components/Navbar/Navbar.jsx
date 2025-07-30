241a070898eef5aefcb970fd6ecdba4ef9fe2d47
Omar <jabour.omar@hotmail.com>
2025-07-30 19:17:32 +0200

    :art: working on  the navlink :/   :art:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,6 +14,10 @@ const Navbar = () => {
         </ul>
         <div className='navbar-right'>
           <img src={assets.search_icon} alt=''/>
+          <div className='navbar-search-icon'>
+            <img src={assets.basket_icon} alt=''/>
+            <div className='dot'></div>
+          </div>
         </div>
     </div>
   )
