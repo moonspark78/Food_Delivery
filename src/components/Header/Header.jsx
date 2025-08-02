fb5044c67f732230258461938cddcdd9a7fabeb8
Omar <jabour.omar@hotmail.com>
2025-08-02 14:53:08 +0200

    :art: working on  the Header component  :art:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,11 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="herder-contents">
-            <h2>Order your favourite food here</h2>
+            <h2>Commandez votre nourriture préférée ici</h2>
+            <p>Choisissez parmi un menu diversifié avec une délicieuse gamme de plats conçus avec les meilleurs ingrédients
+et l'expertise culinaire. Notre mission est de satisfaire vos envies et d'élever votre expérience culinaire.
+            </p>
+            <button>Afficher le menu</button>
         </div>
     </div>
   )
