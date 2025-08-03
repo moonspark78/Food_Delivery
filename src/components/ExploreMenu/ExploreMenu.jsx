fdce8a02037ed07b906bcce081e04d1b576ba2a2
Omar <jabour.omar@hotmail.com>
2025-08-03 13:47:23 +0200

    :art: create the ExploreMenu components  :art:

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,12 +4,21 @@ import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
   return (
-    <div>
+    <div className='explore-menu' id='explore-menu'>
         <h1>Explorez notre menu</h1>
-        <p>Choisissez parmi un menu diversifié avec une délicieuse gamme de plats.
+        <p className='explore-menu-text'>Choisissez parmi un menu diversifié avec une délicieuse gamme de plats.
             Notre mission est de satisfaire vos envies et d'élever votre expérience culinaire,
             un délicieux repas à la fois.
         </p>
+        <div className='explore-menu-list'>
+            {menu_list.map((item, index) => {
+                return(
+                    <div>
+                        
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
