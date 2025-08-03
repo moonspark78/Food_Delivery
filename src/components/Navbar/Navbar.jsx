af1ce2ac7b6978767e46a6f367058e8efcd94b04
Omar <jabour.omar@hotmail.com>
2025-08-03 12:26:32 +0200

    :art: working on  the Header components & css  add animation   :art:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
   const [menu, setMenu] = useState("menu");
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt='' className='logo'/>
+        <img src={assets.logo1} alt='' className='logo'/>
         <ul className='navbar-menu'>
-            <li onClick={() => setMenu("home")} className={menu=== "home" ? "active" : ""}>home</li>
-            <li onClick={() => setMenu("menu")} className={menu=== "menu" ? "active" : ""}>menu</li>
-            <li onClick={() => setMenu("mobile-app")} className={menu=== "mobile-app" ? "active" : ""}>mobile-app</li>
-            <li onClick={() => setMenu("contact-us")} className={menu=== "contact-us" ? "active" : ""}>contact us</li>
+            <li onClick={() => setMenu("home")} className={menu=== "home" ? "active" : ""}>Acceuil</li>
+            <li onClick={() => setMenu("menu")} className={menu=== "menu" ? "active" : ""}>Menu</li>
+            <li onClick={() => setMenu("mobile-app")} className={menu=== "mobile-app" ? "active" : ""}>App-Mobile</li>
+            <li onClick={() => setMenu("contact-us")} className={menu=== "contact-us" ? "active" : ""}>Nous Contacter</li>
         </ul>
         <div className='navbar-right'>
           <img src={assets.search_icon} alt=''/>
