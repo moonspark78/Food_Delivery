36a649d95adb46ca2031effa12c7d426a27b4823
Omar <jabour.omar@hotmail.com>
2025-08-03 13:35:02 +0200

    :art: create the ExploreMenu component ::   :art:

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
   return (
     <div>
-        
+        <h1>Explorez notre menu</h1>
+        <p>Choisissez parmi un menu diversifié avec une délicieuse gamme de plats.
+            Notre mission est de satisfaire vos envies et d'élever votre expérience culinaire,
+            un délicieux repas à la fois.
+        </p>
     </div>
   )
 }
