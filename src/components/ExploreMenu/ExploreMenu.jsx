8f4cf8dd26fbbbdc6f7823ba06a57c2902a0c874
Omar <jabour.omar@hotmail.com>
2025-08-04 19:43:32 +0200

    :art: working  the ExploreMenu components ::  :art:

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -13,7 +13,7 @@ const ExploreMenu = ({category, setCategory}) => {
         <div className='explore-menu-list'>
             {menu_list.map((item, index) => {
                 return(
-                    <div onClick={() => setCategory()} key={index} className='explore-menu-list-item'>
+                    <div onClick={() => setCategory(prev=>prev===item.menu_name ? 'All' : item.menu_name)} key={index} className='explore-menu-list-item'>
                         <img src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
