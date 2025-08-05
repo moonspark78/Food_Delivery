51b80cdb81416b75b0ac93286c2c3565ea511b6d
Omar <jabour.omar@hotmail.com>
2025-08-05 20:33:36 +0200

    :art: working  the ExploreMenu component :art:

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -86,15 +86,15 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Salade",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Rouleaux",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Déserts",
         menu_image: menu_3
     },
     {
@@ -102,251 +102,251 @@ export const menu_list = [
         menu_image: menu_4
     },
     {
-        menu_name: "Cake",
+        menu_name: "Gâteau",
         menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
+        menu_name: "Veg pur",
         menu_image: menu_6
     },
     {
-        menu_name: "Pasta",
+        menu_name: "Pâtes",
         menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
+        menu_name: "Nouilles",
         menu_image: menu_8
     }]
 
 export const food_list = [
     {
         _id: "1",
-        name: "Greek salad",
+        name: "salade grecque",
         image: food_1,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Salad"
     },
     {
         _id: "2",
-        name: "Veg salad",
+        name: "Salade de légumes",
         image: food_2,
         price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Salad"
     }, {
         _id: "3",
         name: "Clover Salad",
         image: food_3,
         price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Salad"
     }, {
         _id: "4",
-        name: "Chicken Salad",
+        name: "Salade de poulet",
         image: food_4,
         price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Salad"
     }, {
         _id: "5",
-        name: "Lasagna Rolls",
+        name: "Lasagne",
         image: food_5,
         price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Rolls"
     }, {
         _id: "6",
-        name: "Peri Peri Rolls",
+        name: "Peri peri rolls",
         image: food_6,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Rolls"
     }, {
         _id: "7",
         name: "Chicken Rolls",
         image: food_7,
         price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Rolls"
     }, {
         _id: "8",
-        name: "Veg Rolls",
+        name: "Végétaux",
         image: food_8,
         price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Rolls"
     }, {
         _id: "9",
-        name: "Ripple Ice Cream",
+        name: "Crème d'ondulation",
         image: food_9,
         price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Deserts"
     }, {
         _id: "10",
-        name: "Fruit Ice Cream",
+        name: "Glace aux fruits",
         image: food_10,
         price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Deserts"
     }, {
         _id: "11",
-        name: "Jar Ice Cream",
+        name: "Crème glacée",
         image: food_11,
         price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Deserts"
     }, {
         _id: "12",
-        name: "Vanilla Ice Cream",
+        name: "Glace à la vanille",
         image: food_12,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Deserts"
     },
     {
         _id: "13",
-        name: "Chicken Sandwich",
+        name: "Sandwich au poulet",
         image: food_13,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Sandwich"
     },
     {
         _id: "14",
-        name: "Vegan Sandwich",
+        name: "Sandwich végétalien",
         image: food_14,
         price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Sandwich"
     }, {
         _id: "15",
-        name: "Grilled Sandwich",
+        name: "Sandwich grillé",
         image: food_15,
         price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Sandwich"
     }, {
         _id: "16",
-        name: "Bread Sandwich",
+        name: "Sandwich au pain",
         image: food_16,
         price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Sandwich"
     }, {
         _id: "17",
-        name: "Cup Cake",
+        name: "Gâteau",
         image: food_17,
         price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Cake"
     }, {
         _id: "18",
-        name: "Vegan Cake",
+        name: "Gâteau végétalien",
         image: food_18,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Cake"
     }, {
         _id: "19",
-        name: "Butterscotch Cake",
+        name: "Gâteau au caramel",
         image: food_19,
         price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Cake"
     }, {
         _id: "20",
-        name: "Sliced Cake",
+        name: "Gâteau tranché",
         image: food_20,
         price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Cake"
     }, {
         _id: "21",
-        name: "Garlic Mushroom ",
+        name: "Champignon à l'ail",
         image: food_21,
         price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pure Veg"
     }, {
         _id: "22",
-        name: "Fried Cauliflower",
+        name: "Chou-fleur frit",
         image: food_22,
         price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pure Veg"
     }, {
         _id: "23",
-        name: "Mix Veg Pulao",
+        name: "Mélanger le Veg Pulao",
         image: food_23,
         price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pure Veg"
     }, {
         _id: "24",
-        name: "Rice Zucchini",
+        name: "Courgettes de riz",
         image: food_24,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pure Veg"
     },
     {
         _id: "25",
-        name: "Cheese Pasta",
+        name: "Pâtes au fromage",
         image: food_25,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pasta"
     },
     {
         _id: "26",
-        name: "Tomato Pasta",
+        name: "Pâtes à tomate",
         image: food_26,
         price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pasta"
     }, {
         _id: "27",
-        name: "Creamy Pasta",
+        name: "Pâtes crémeuses",
         image: food_27,
         price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pasta"
     }, {
         _id: "28",
-        name: "Chicken Pasta",
+        name: "Pâtes de poulet",
         image: food_28,
         price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Pasta"
     }, {
         _id: "29",
-        name: "Buttter Noodles",
+        name: "Nouilles au beurre",
         image: food_29,
         price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Noodles"
     }, {
         _id: "30",
-        name: "Veg Noodles",
+        name: "Nouilles de légumes",
         image: food_30,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Noodles"
     }, {
         _id: "31",
-        name: "Somen Noodles",
+        name: "Nouilles fries",
         image: food_31,
         price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Noodles"
     }, {
         _id: "32",
-        name: "Cooked Noodles",
+        name: "Nouilles cuites",
         image: food_32,
         price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "La nourriture fournit des nutriments essentiels pour la santé et le bien-être globaux",
         category: "Noodles"
     }
 ]
