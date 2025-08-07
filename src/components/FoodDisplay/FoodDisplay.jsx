1d244d16d946b60be504d6213e338378d126a344
Omar <jabour.omar@hotmail.com>
2025-08-07 22:15:00 +0200

    :art: create the FoodDisplay compoennents  :art:

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 
 const FoodDisplay = () => {
+
+    const { food_list } = useContext(StoreContext);
+
+
   return (
     <div>FoodDisplay</div>
   )
