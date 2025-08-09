55f3a829f0a6b8ba5cc272c882dc79e9db0e2703
Omar <jabour.omar@hotmail.com>
2025-08-09 18:10:56 +0200

    :art: create the FoodItem  compoennent  :art:

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -3,8 +3,11 @@ import './FoodItem.css'
 
 const FoodItem = ({id,name,price,description,image}) => {
   return (
-    <div>
-
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            <img className='food-item-image' src={image} alt={name}/>
+        
+        </div>
     </div>
   )
 }
