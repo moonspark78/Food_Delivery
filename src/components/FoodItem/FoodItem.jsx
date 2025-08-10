8f66cac96f0c0d2a59fc7ae17427d44040ff57cc
Omar <jabour.omar@hotmail.com>
2025-08-10 18:02:35 +0200

    :art: create the FoodItem  compoennents :/;  :art:

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './FoodItem.css'
+import { assets } from '../../assets/assets'
 
 const FoodItem = ({id,name,price,description,image}) => {
   return (
@@ -10,6 +11,7 @@ const FoodItem = ({id,name,price,description,image}) => {
         <div className="food-item-info">
           <div className="food-item-name-rating">
             <p>{name}</p>
+            <img src={assets.rating_starts} alt="" />
           </div>
         </div>
     </div>
